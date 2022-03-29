19d27fcdb4403e2039f83a8f3f1edf0288f7547e
qkrwhddms <qkrwhddms01@gmail.com>
2022-03-29 12:03:34 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/service/IMelonService.java
@@ -0,0 +1,38 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MelonDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IMelonService {
+
+
+    /**
+     * 멜론 노래 리스트 저장하기
+     */
+    int collectMelonSong() throws Exception;
+
+    /**
+     * 오늘 수집된 멜론 노래리스트 가져오기
+     */
+    List<MelonDTO> getSongList() throws Exception;
+
+    /**
+     * 멜론 가수별 노래 수 가져오기
+     */
+    List<MelonDTO> getSingerSongCnt() throws Exception;
+
+    /**
+     * 가수의 노래 가져오기
+     *
+     * @return 노래 리스트
+     */
+    List<MelonDTO> getSingerSong() throws Exception;
+
+    /**
+     * 멜론 노래 리스트 한번에 저장하기
+     */
+    int collectMelonSongMany() throws Exception;
+
+}
